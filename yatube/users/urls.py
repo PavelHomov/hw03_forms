55f6990f2e4dae9e5bca175f0ae973001ad4bda0
Pavel Homov <pawel.khomov@yandex.ru>
2022-08-16 23:17:58 +0300

    pep8

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -3,18 +3,17 @@ from django.contrib.auth.views import PasswordChangeDoneView
 from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView
 from django.contrib.auth.views import PasswordResetConfirmView
 from django.contrib.auth.views import PasswordResetCompleteView
-from django.urls import path, reverse_lazy
+from django.urls import path
 
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path(
-      'logout/',
-      LogoutView.as_view(template_name='users/logged_out.html'),
-      name='logout'
-    ),
+    path('logout/',
+         LogoutView.as_view(template_name='users/logged_out.html'),
+         name='logout'
+         ),
     path(
         'login/',
         LoginView.as_view(template_name='users/login.html'),
@@ -22,12 +21,14 @@ urlpatterns = [
     ),
     path(
         'password_change/',
-        PasswordChangeView.as_view(template_name='users/password_change_form.html'),
+        PasswordChangeView.as_view(
+            template_name='users/password_change_form.html'),
         name='password_change_form'
     ),
     path(
         'password_change/done/',
-        PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
+        PasswordChangeDoneView.as_view(
+            template_name='users/password_change_done.html'),
         name='password_change/done'
     ),
     path(
