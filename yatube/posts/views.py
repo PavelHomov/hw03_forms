6ad54310607afc20f0637eb438df877c7421c968
Pavel Homov <pawel.khomov@yandex.ru>
2022-08-18 20:57:49 +0300

    pep8 fix

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 
 from .forms import PostForm
 from .models import Post, Group, User
-from .constants import POSTS_PAGE
 from .utils import paginator
 
 
